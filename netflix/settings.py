16de16e470c81688b81eac0b04aedc1557a2c1ea
Felipe Menezes Lins <felipemenezesmk22@gmail.com>
2025-05-01 10:29:12 -0300

    allowed liberado

--- netflix/settings.py
+++ netflix/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-82npo7q&j63^%=z(h!-oku!x0756$n0!6(#z+c7l5i@q@2#uis
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
 
 
 
