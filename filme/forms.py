5f319c51ad441f1efc2a502abd8eb215862f4369
Felipe Menezes Lins <felipemenezesmk22@gmail.com>
2025-05-17 21:32:59 -0300

    atualização no footer

--- filme/forms.py
+++ filme/forms.py
@@ -3,11 +3,24 @@ from django import forms
 from .models import Usuario
 
 class CriandoUsuario(UserCreationForm):
-    email = forms.EmailField()
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={'placeholder': 'Email'})
+    )
+    password1 = forms.CharField(
+        label='Senha',
+        widget=forms.PasswordInput(attrs={'placeholder': 'Senha'})
+    )
+    password2 = forms.CharField(
+        label='Confirmação de senha',
+        widget=forms.PasswordInput(attrs={'placeholder': 'Confirmar senha'})
+    )
 
     class Meta:
         model = Usuario
         fields = ('username', 'email', 'password1', 'password2')
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder': 'Nome de usuário'}),
+    }
 
 class Emailenter(forms.Form):
     email = forms.EmailField(label=False, widget=forms.TextInput(attrs={'placeholder': 'Email'}))
